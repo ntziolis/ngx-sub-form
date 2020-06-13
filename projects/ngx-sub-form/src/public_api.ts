0ece3e6d7ab2363df9af4138f706185bc10a611d
ntziolis <nikolas@tziolis.de>
2020-06-13 10:56:35 +0200

    feat(sub-form-module): created module for using sub-form-directive

--- projects/ngx-sub-form/src/public_api.ts
+++ projects/ngx-sub-form/src/public_api.ts
@@ -8,4 +8,5 @@ export * from './lib/ngx-sub-form.component';
 export * from './lib/ngx-root-form.component';
 export * from './lib/ngx-automatic-root-form.component';
 export * from './lib/ngx-sub-form.types';
-export * from './lib/ngx-sub-form.decorators';
+export * from './lib/sub-form.directive';
+export * from './lib/ngx-sub-form.module';
